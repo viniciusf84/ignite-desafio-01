b663dff1bc26978ae44c20bb11f8eaaa6660db14
inicius <viniciusf84@hotmail.com>
2021-05-30 00:31:59 -0300

    Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import '../styles/tasklist.scss'
+import '../styles/tasklist.scss';
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare } from 'react-icons/fi';
 
 interface Task {
   id: number;
@@ -15,15 +15,34 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const brandNewTask = {
+      id: Math.random(), 
+      title: newTaskTitle, 
+      isComplete: false
+    }
+    
+    setTasks(oldTasks => ([...oldTasks, brandNewTask]));
+    setNewTaskTitle('');
+
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(item => {
+      if(item.id === id) {
+        return {...item, isComplete: !item.isComplete}
+      }
+      return item;
+    });
+    
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(tasksItem => tasksItem.id !== id);
+
+    setTasks(filteredTasks);
   }
 
   return (
